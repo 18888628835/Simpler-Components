4d5b27e00a0544e2badb315d6c390992bba3fdfa
qiuyanxi <qiuyanxi1992@gmail.com>
2022-04-22 10:48:47 +0800

    完善 resizableBox 的代码

--- src/resizable-box/index.tsx
+++ src/resizable-box/index.tsx
@@ -2,10 +2,11 @@
  * @Author: 邱彦兮
  * @Date: 2022-04-21 16:49:42
  * @LastEditors: 邱彦兮
- * @LastEditTime: 2022-04-21 23:48:13
+ * @LastEditTime: 2022-04-22 10:42:53
  * @FilePath: /Simpler-Components/src/Resizable-box/index.tsx
  */
-import React, { useEffect, useRef, useCallback, useState } from 'react';
+import { getCoords } from '@/utils/helper';
+import React, { useEffect, useRef, useCallback, FC } from 'react';
 import { Wrap } from './style';
 
 type Direction =
@@ -18,11 +19,28 @@ type Direction =
   | 'left'
   | 'right';
 
-const ResizableBox = (props) => {
-  let { minWidth = 30, minHeight = 30 } = props;
+interface ResizableBoxProps {
+  /**
+   * @default false
+   * @description 是否隐藏
+   */
+  hidden: boolean;
+  /**
+   * @default 20
+   * @description  盒子收缩后最小宽度
+   */
+  minWidth: number;
+  /**
+   * @default 20
+   * @description  盒子收缩后最小高度
+   */
+  minHeight: number;
+}
+const ResizableBox: FC<ResizableBoxProps> = (props) => {
+  let { minWidth = 20, minHeight = 20, hidden = false } = props;
   const box = useRef<HTMLDivElement | null>(null);
   const direction = useRef('');
-  const rectProperties = useRef({
+  const propertiesRecord = useRef({
     left: 0,
     top: 0,
     width: 0,
@@ -33,63 +51,10 @@ const ResizableBox = (props) => {
     pointY: 0,
   });
 
-  const onMouseDown = useCallback((currentDirection: Direction) => {
-    let [left, top, width, height] = [
-      box.current?.offsetLeft!,
-      box.current?.offsetTop!,
-      box.current?.offsetWidth!,
-      box.current?.offsetHeight!,
-    ];
-    let [right, bottom] = [left + width, top + height];
-    let pointX, pointY;
-    switch (currentDirection) {
-      case 'left_top':
-        pointX = left;
-        pointY = top;
-        break;
-      case 'left_bottom':
-        pointX = left;
-        pointY = bottom;
-        break;
-      case 'right_top':
-        pointX = right;
-        pointY = top;
-        break;
-      case 'right_bottom':
-        pointX = right;
-        pointY = bottom;
-        break;
-      case 'top':
-        pointY = top;
-        break;
-      case 'bottom':
-        pointY = bottom;
-        break;
-      case 'left':
-        pointX = left;
-
-        break;
-      case 'right':
-        pointX = right;
-        break;
-    }
-    rectProperties.current = {
-      left,
-      top,
-      width,
-      height,
-      pointX,
-      pointY,
-      right,
-      bottom,
-    };
-    direction.current = currentDirection;
-    document.addEventListener('mousemove', onMouseMove);
-  }, []);
   const onMouseMove = useCallback((e) => {
-    let { left, top, width, height, right, bottom } = rectProperties.current;
-    let offsetX = rectProperties.current.pointX - e.pageX;
-    let offsetY = rectProperties.current.pointY - e.pageY;
+    let { left, top, width, height, right, bottom } = propertiesRecord.current;
+    let offsetX = propertiesRecord.current.pointX - e.clientX;
+    let offsetY = propertiesRecord.current.pointY - e.clientY;
 
     switch (direction.current) {
       case 'left_top':
@@ -133,6 +98,30 @@ const ResizableBox = (props) => {
     box.current!.style.height = `${Math.max(height, minHeight)}px`;
   }, []);
 
+  const onMouseDown = useCallback((e, currentDirection: Direction) => {
+    // 获取元素在文档中的点位
+    let { left, right, top, bottom } = getCoords(box.current);
+    // 获取元素矩阵大小
+    let { width, height } = box.current?.getBoundingClientRect()!;
+    // 存下鼠标点击后的鼠标坐标
+    let pointX = e.clientX,
+      pointY = e.clientY;
+    // 将所有属性都记录下来
+    Object.assign(propertiesRecord.current, {
+      left,
+      top,
+      width,
+      height,
+      pointX,
+      pointY,
+      right,
+      bottom,
+    });
+
+    direction.current = currentDirection;
+    document.addEventListener('mousemove', onMouseMove);
+  }, []);
+
   const onMouseUp = useCallback(() => {
     document.removeEventListener('mousemove', onMouseMove);
   }, []);
@@ -145,7 +134,7 @@ const ResizableBox = (props) => {
   }, []);
 
   return (
-    <Wrap>
+    <Wrap hidden={hidden}>
       <div className="resizable_box" ref={box}>
         {[
           'left_top',
@@ -160,7 +149,7 @@ const ResizableBox = (props) => {
           <div
             key={item}
             className={`rect rect_${item}`}
-            onMouseDown={() => onMouseDown(item as Direction)}
+            onMouseDown={(e) => onMouseDown(e, item as Direction)}
           ></div>
         ))}
       </div>
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,7 +2,7 @@
  * @Author: 邱彦兮
  * @Date: 2022-03-26 09:14:16
  * @LastEditors: 邱彦兮
- * @LastEditTime: 2022-03-26 09:25:12
+ * @LastEditTime: 2022-04-22 09:54:21
  * @FilePath: /Simpler-Components/src/utils/helper.ts
  */
 export function throttle(handler, delay) {
@@ -30,5 +30,24 @@ export function debounce(handler, delay) {
       handler.call(context, ...args);
       clearTimeout(timer);
     }, delay);
+  };
+}
+
+/**
+ * 获取元素在整个文档中的位置
+ * @param elem 元素
+ * @returns 返回元素在文档中的left、right、top、bottom 位置
+ */
+export function getCoords(elem) {
+  const rect = elem.getBoundingClientRect();
+  let left = window.pageXOffset + rect.left;
+  let right = window.pageXOffset + rect.right;
+  let top = window.pageYOffset + rect.top;
+  let bottom = window.pageYOffset + rect.bottom;
+  return {
+    left,
+    right,
+    top,
+    bottom,
   };
 }
